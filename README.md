fe3de913d7612b62065eb0d11f9cd5fa73275999
Brian Clapper <bmc@clapper.org>
2013-10-04 10:16:22 -0400

    First working version

--- README.md
+++ README.md
@@ -1,28 +1,91 @@
-A simple Minecraft Bukkit server plugin that allows users to change their
-display names. Written primarily to play around with Josh Cough's
+RandomTeleport is yet another Minecraft Bukkit server plugin that allows
+users to teleport somewhere randomly within the current world.
+
+There are other, similar, plugins already out there, including:
+
+* [RandomPort](http://dev.bukkit.org/bukkit-plugins/randomport/)
+* [Random Teleport](http://dev.bukkit.org/bukkit-plugins/random-teleport/)
+* [Teleport Random Location](http://dev.bukkit.org/bukkit-plugins/teleport-random-location/)
+* [Random Location Teleporter](http://dev.bukkit.org/bukkit-plugins/randomlocationteleporter/)
+
+So, why implement yet another one? Two reasons, primarily:
+
+* Guaranteed access to the source code. The source to this version is
+  easy to find and will be available as long as the plugin is around.
+* It serves as a reasonable template for other plugins.
+
+This plugin is written in Scala and uses Josh Cough's
 [Scala Bukkit plugin API](https://github.com/joshcough/MinecraftPlugins).
 
-## Building
+# Getting the Plugin
+
+Currently, the only way to get the plugin is to build it from source. To do
+that, you'll need the following:
+
+* A Java JDK (preferably Java 7).
+* A recent version of [Gradle](http://gradle.org).
+
+## Building the Plugin
 
 * Check out the repo.
-* You'll need a recent version of [Gradle](http://gradle.org)
 * Run: `gradle zip`
-* The resulting distribution is in `build/distributions/nickname.zip`.
+* The resulting distribution is in `build/distributions/mc-random-teleport-VERSION.zip`,
+  where `VERSION` is the current version of the plugin.
 
-## Installation
+## Installing the Plugin
 
-Unpack the `nickname.zip` file you built above. The zip contains three jar
-files:
+Unpack the zip file you built above.  Unzipping will result in an
+`mc-random-teleport` subdirectory containing numerous jar files. Assuming the
+top-level directory of your Minecraft world is `$WORLD`, issue the following
+commands. (These commands are suitable for Unix and Mac systems. If you're on
+Windows, either use the appropriate Windows commands or run the following
+commands from a [Cygwin](http://www.cygwin.com/) shell.)
 
-- The Scala Library plugin, which provides the Scala runtime library to
-  Scala Bukkit plugins.
-- The Scala Plugin API jar file.
-- `nickname.jar`, which contains the code for this plugin.
+    $ cd mc-endless-dispenser
+    $ mkdir -p $WORLD/lib
+    $ cp scala-library-2.10.jar $WORLD/lib
+    $ cp mclib*.jar $WORLD/lib/mclib.jar
+    $ cp scala-minecraft-plugin-api*.jar RandomTeleport.jar $WORLD/plugins
 
-Copy all three jars to your server's `plugins` directory, adjust any
-permissions (see below), and restart or reload your server.
+Then, restart or reload your Bukkit server.
 
-## Permissions
+# Configuration
+
+## Commands
+
+The plugin provides an in-game `/rp` chat command. When invoked, the plugin
+randomly chooses a location within the user's current world and teleports
+the user to that world.
+
+## Cool-down period
+
+The avoid an excessive chunk-loading drain on the server, the plugin supports
+a cool-down period. After a user uses `/rp`, he or she can't use it again for
+a configurable number of seconds. (See **Configuration**, below.)
+
+## Placement
+
+A user will never be randomly teleported into water, lava, or air. The
+plugin randomly chooses _x_ and _z_ coordinates within the current world.
+It then selects the _y_ (elevation) coordinate corresponding to the topmost
+non-air block at that (_x_, _z_) location.
+
+## Configuration
+
+Configuration is self-explanatory. When the plugin comes up, it creates
+a `plugins/RandomTeleport` directory, if that directory doesn't exist. If
+you want to override the default configuration, download a copy of
+`src/main/resources/config.yml` from this repo, edit it, install it in that
+directory, and restart or reload your server. The comments in the `config.yml`
+file explain what each configuration item does.
+
+## Plugin Permissions
+
+The plugin currently doesn't support any permissions. It's either on or off.
+If the plugin is enabled, any user can use it. Adding permissions is a future
+enhancement.
+
+# Permissions
 
 It's not necessary to set permissions, because the plugin assumes
 reasonable defaults. However, if you're using a permissions plugin such
--- src/main/scala/org/clapper/minecraft/RandomTeleport.scala
+++ src/main/scala/org/clapper/minecraft/RandomTeleport.scala
@@ -1,11 +1,19 @@
 package org.clapper.minecraft.randomteleport
 
-import jcdc.pluginfactory.{ScalaPlugin, CommandPlugin, ListenersPlugin}
+import com.joshcough.minecraft.{ScalaPlugin, CommandPlugin, ListenersPlugin}
+
+import org.clapper.minecraft.lib.Implicits.Player._
+import org.clapper.minecraft.lib.Implicits.Logging._
+import org.clapper.minecraft.lib.Implicits.Block._
+import org.clapper.minecraft.lib.Listeners._
+import org.clapper.minecraft.lib.{PluginLogging, ScalaPluginExtras}
 
 import org.bukkit.entity.Player
+import org.bukkit.block.Block
+import org.bukkit.World
 import org.bukkit.metadata.MetadataValue
 import org.bukkit.plugin.Plugin
-import org.bukkit.Location
+import org.bukkit.{Material, Location}
 
 import scala.language.implicitConversions
 import scala.annotation.tailrec
@@ -13,52 +21,16 @@ import scala.collection.JavaConverters._
 
 import java.security.SecureRandom
 
-private class EnrichedPlayer(val player: Player) {
-  def notice(message: String) = {
-    player.sendRawMessage(s"§e${message}")
-  }
+private object Constants {
+  val HeightDelta         = 10 // location ok if lower than (max height - this)
+  val LastTimeMetadataKey = "last-random-teleport-time"
+  val CanRandomlyTeleport = "rp.teleport"
+  val DefaultElapsedTime  = 60 * 60 * 1000 // 1 hour in milliseconds
 }
 
-private object Implicits {
-  implicit def playerToEnrichedPlayer(p: Player) = new EnrichedPlayer(p)
-  implicit def enrichedPlayerToPlayer(e: EnrichedPlayer) = e.player
-}
-
-trait RandomTeleportConstants {
-  val MIN_X               = -10000
-  val MIN_Z               = -10000
-  val MAX_X               = 10000
-  val MAX_Z               = 10000
-  val HEIGHT_DELTA        = 10 // location ok if lower than (max height - this)
-  val MAX_ATTEMPTS        = 10
-  val METADATA_KEY        = "last-random-teleport-time"
-  val MIN_TIME_DELTA      = 120 * 1000 // 2 minutes, as ms
-  val CAN_RANDOM_TELEPORT = "rp.teleport"
-}
-
-trait Logging {
-  self: ScalaPlugin =>
-
-  private lazy val logger = getLogger()
-
-  def logMessage(msg: String) = {
-    // logger.info(s"[$name] $msg")
-    logger.info(s"$msg")
-  }
-}
-
-trait RandomTeleportPermissions extends Logging with RandomTeleportConstants {
-  self: ScalaPlugin =>
-
-  def ifPermittedToRandomTeleport(player: Player)(code: => Unit) = {
-    if (! player.isPermissionSet(CAN_RANDOM_TELEPORT)) {
-      code
-    }
-
-    else {
-      logMessage(s"Player ${player.name} isn't permitted to random-teleport.")
-      player.sendError("You aren't permitted teleport randomly.")
-    }
+private object Permissions {
+  def canRandomlyTeleport(player: Player): Boolean = {
+    true
   }
 }
 
@@ -79,13 +51,14 @@ private case class RTPMetaData(name: String, plugin: Plugin) extends MetadataVal
 class RandomTeleportPlugin
   extends ListenersPlugin
   with    CommandPlugin
-  with    Logging
-  with    RandomTeleportConstants
-  with    RandomTeleportPermissions {
+  with    PluginLogging {
 
-  private lazy val random = new SecureRandom()
+  private lazy val random = SecureRandom.getInstance("SHA1PRNG", "SUN")
+  // Force random number generator to see itself. See
+  // https://www.cigital.com/justice-league-blog/2009/08/14/proper-use-of-javas-securerandom/
+  random.nextInt()
 
-  import Implicits._
+  private lazy val configuration = ConfigData(this)
 
   val listeners = List(
     OnPlayerJoin { (player, event) =>
@@ -94,41 +67,60 @@ class RandomTeleportPlugin
   )
 
   val command = Command("rp", "Randomly teleport in the world") { case player =>
-    ifPermittedToRandomTeleport(player) {
+    if (Permissions.canRandomlyTeleport(player)) {
       teleport(player)
     }
   }
 
+  override def onEnable(): Unit = {
+    super.onEnable()
+    val dataDirectory = this.getDataFolder
+    if (! dataDirectory.exists) {
+      logMessage(s"Creating $dataDirectory")
+      dataDirectory.mkdirs()
+    }
+  }
+
   private def teleport(player: Player) {
-    val world          = player.world
-    val maxWorldHeight = world.getMaxHeight
-    val maxElevation   = maxWorldHeight - 10
-    val now            = System.currentTimeMillis
+    val world = player.world
+    val now   = System.currentTimeMillis
 
     @tailrec def successfullyTeleported(i: Int): Boolean = {
-      if (i > MAX_ATTEMPTS) {
+      if (i >= configuration.MaxAttempts) {
         logMessage(s"Failed to teleport ${player.name} after ${i} attempts.")
         player.sendError(s"Unable to teleport you after ${i} attempts.")
         false
       }
 
       else {
-        val x = randomCoordinate(MIN_X, MAX_X)
-        val z = randomCoordinate(MIN_Z, MAX_Z)
+        val (min, max) = (configuration.MinCoordinate,
+                          configuration.MaxCoordinate)
+        val x     = randomCoordinate(min, max)
+        val z     = randomCoordinate(min, max)
         val world = player.world
-        val y = world.getHighestBlockYAt(x, z)
-        val succeeded = if (y < maxElevation) {
-          val loc = new Location(world, x.toDouble, y.toDouble, z.toDouble)
-          val ok = player.teleport(loc)
+        val block = highestNonAirBlock(world, x, z)
+        val loc   = block.loc
+        val y     = loc.y.toInt
+        logMessage(s"Highest block at ${block.loc.x}, ${block.loc.z} is $block")
+
+        val succeeded = if (landingPointOkay(block, world.getMaxHeight - 10)) {
+          // Teleport one above the location.
+          val newLoc = new Location(world,
+                                    loc.x.toDouble,
+                                    (loc.y + 1).toDouble,
+                                    loc.z.toDouble)
+          val ok = player.teleport(newLoc)
           if (! ok)
             logMessage("Failed to teleport player ${player.name} to ($x, $z)")
           else
-            player.setMetadata(METADATA_KEY,
+            player.setMetadata(Constants.LastTimeMetadataKey,
                                RTPMetaData(now.toString, this))
           ok
         }
+
         else {
-          logMessage("Can't teleport player to ($x, $y): Too high.")
+          logMessage(s"Can't teleport player to (x=$x, y=$y, z=$z): " +
+                     s"Block ${block} isn't a suitable location.")
           false
         }
 
@@ -139,31 +131,59 @@ class RandomTeleportPlugin
       }
     }
 
-    val elapsed = now - lastRandomTeleportTime(player)
-    if (elapsed < MIN_TIME_DELTA) {
-      val left = MIN_TIME_DELTA - elapsed
+    val lastTeleported = lastRandomTeleportTime(player)
+    val elapsed = now - lastTeleported
+    logMessage(s"timeBetween=${configuration.TimeBetweenTeleports}, elapsed=${elapsed}, last=${lastTeleported}, now=${now}")
+    if (elapsed < configuration.TimeBetweenTeleports) {
+      val left = (configuration.TimeBetweenTeleports - elapsed) / 1000
       val sec  = if (left == 1) "second" else "seconds"
-      player.notice("You can't teleport for another $left ${sec}.")
+      player.notice(s"You can't teleport for another $left ${sec}.")
     }
 
     else {
-      successfullyTeleported(0)
+      if (successfullyTeleported(0)) {
+        val loc = player.loc
+        val sLoc = s"(${loc.x}, ${loc.y}, ${loc.z})"
+        logMessage(s"Teleported ${player.name} to $sLoc")
+        player.sendRawMessage(s"You have been teleported to $sLoc")
+      }
     }
   }
 
+  private def highestNonAirBlock(world: World, x: Int, z: Int): Block = {
+
+    @tailrec def findNonAirBlock(y: Int): Block = {
+      val block = world.blockAt(x, y, z)
+      if (block.getType != Material.AIR)
+        block
+      else
+        findNonAirBlock(y - 1)
+    }
+
+    findNonAirBlock(world.getHighestBlockYAt(x, z))
+  }
+
+  private def landingPointOkay(block: Block, maxElevation: Int): Boolean = {
+    (block.loc.y.toInt < maxElevation) && (blockOkay(block))
+  }
+
+  private def blockOkay(block: Block): Boolean = {
+    (block.getType != Material.WATER) &&
+    (block.getType != Material.STATIONARY_WATER) &&
+    (block.getType != Material.LAVA) &&
+    (block.getType != Material.STATIONARY_LAVA) &&
+    (block.getType != Material.FIRE)
+  }
+
   private def lastRandomTeleportTime(player: Player) = {
-    player.getMetadata(METADATA_KEY).asScala.toList match {
-      case Nil       => System.currentTimeMillis
-      case m :: tail => java.lang.Long.parseLong(m.asInstanceOf[MetadataValue].asString)
+    val now = System.currentTimeMillis
+    player.getMetadata(Constants.LastTimeMetadataKey).asScala.toList match {
+      case Nil                        =>  now - Constants.DefaultElapsedTime
+      case (m: MetadataValue) :: tail => java.lang.Long.parseLong(m.asString)
     }
   }
 
   private def randomCoordinate(min: Int, max: Int): Int = {
     random.nextInt(max - min) + min
-  }
-
-  override def onEnable(): Unit = {
-    super.onEnable()
-    logMessage("I'm alive!")
   }
 }
